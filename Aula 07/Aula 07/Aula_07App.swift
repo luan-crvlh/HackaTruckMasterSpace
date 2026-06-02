3c5a8d0d485deb4c77f2a30ad80055ef38bf40f0
Luanderson Carvalho <luanderson.carvalho@ufpi.edu.br>
2026-06-02 18:04:55 -0300

    aula 07

---
+++ Aula 07/Aula 07/Aula_07App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_07App.swift
+//  Aula 07
+//
+//  Created by Turma02-23 on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Aula_07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aula 07/Aula 07/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aula 07
+//
+//  Created by Turma02-23 on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
